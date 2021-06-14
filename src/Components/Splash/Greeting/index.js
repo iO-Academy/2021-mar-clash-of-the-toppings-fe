3ef13d51caa1ac9973a93f87ca49aef00062a8db
Toby Williams <toby.williams@designatron.com>
2021-06-14 14:23:59 +0100

    css applied to greeter and welcomeText

--- src/Components/Splash/Greeting/index.js
+++ src/Components/Splash/Greeting/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Greeting.css';
 
-const Greeting = () => <h1>CLASH OF THE TOPPINGS</h1>
+const Greeting = () => <h1>CLASH OF THE TOPPINGS!</h1>
 
 export default Greeting
