e4f6786358b43558830d74c12aa125665ef5ba9a
Toby Williams <toby.williams@designatron.com>
2021-06-14 15:11:27 +0100

    finished welcomeText styling

--- src/Components/Splash/WelcomeText/index.js
+++ src/Components/Splash/WelcomeText/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import './WelcomeText.css';
 
-const WelcomeText = () => <h4>Calling all pizza lovers! We want to discover the greatest pizza toppings of all time! Press play and choose your favourite topping from the eleven clashes.</h4>
+const WelcomeText = () => <div id='welcomeText'>
+    <h4>Calling all pizza lovers!</h4>
+    <h4>We want to discover the greatest pizza toppings of all time! Press play and choose your favourite topping from the eleven clashes.</h4>
+</div>
 
 export default WelcomeText
