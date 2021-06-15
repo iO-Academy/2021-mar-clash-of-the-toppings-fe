e54de177a21b6b6f0596d6957d5f69f5db545c55
Nick <nickclifford@gmail.com>
2021-06-15 09:44:23 +0100

    added Images folder to src

---
+++ src/Components/Clash/ClashBox/ToppingOneOuter/ToppingOneImg/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SpicyBeef from '././Images/spicyBeef.png'
+
+class BeefImg extends React.Component {
+    render() {
+        return (
+            <div>
+                <img alt="Spicy Beef" src={SpicyBeef}/>
+            </div>
+        )
+    }
+}
+
+export default BeefImg
--- src/Components/Clash/index.js
+++ src/Components/Clash/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import 'Clash.css';
 import ClashTitle from "./ClashTitle";
+// import BeefImg from "./ClashBox/ToppingOneOuter/ToppingOneImg";
 
 class Clash extends React.Component {
     render() {
         return (
             <div>
                 <ClashTitle />
+                {/*<BeefImg />*/}
                 {/*<ClashBox />*/}
             </div>
 
