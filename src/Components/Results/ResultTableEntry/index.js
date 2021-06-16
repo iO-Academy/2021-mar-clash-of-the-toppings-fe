7c1e4affb5f814d0bf9b4ae840ef1c19cade9b04
Zedonk1995 <paulRenglish@yahoo.co.uk>
2021-06-16 10:08:09 +0100

    added % to the win percent text

--- src/Components/Results/ResultTableEntry/index.js
+++ src/Components/Results/ResultTableEntry/index.js
@@ -8,7 +8,7 @@ const ResultTableEntry = (props) => {
             <strong>{props.resultTableEntryIndex + 1}</strong>
             <img src={SpicyBeef} alt='spicy'/>
             <div className='toppingName'>{props.resultTableEntryTopping.name}</div>
-            <div className='winPercentile'>{props.resultTableEntryTopping.winPercent}</div>
+            <div className='winPercentile'>{props.resultTableEntryTopping.winPercent}%</div>
         </div>
     )
 }
