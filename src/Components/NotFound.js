69047e05519a68af1c63e3f04f2ec056229fc747
Nick <nickclifford@gmail.com>
2021-06-17 09:50:46 +0100

    Changed 404 message

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,7 +1,8 @@
 import React from 'react'
+
 class NotFound extends React.Component {
     render() {
-        return <h1>404 - Not found!</h1>
+        return <h1>404 - Pizza Not found!</h1>
     }
 }
 export default NotFound
