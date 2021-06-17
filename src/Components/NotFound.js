806199f8565ace620b4c9d449cac1e264b6442a5
Nick <nickclifford@gmail.com>
2021-06-17 13:48:36 +0100

    Added a go home button on 404 page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-class NotFound extends React.Component {
-    render() {
-        return <h1>404 - Pizza Not found!</h1>
-    }
+const NotFound = () => {
+    return ( <div>
+        <h1>404 - Pizza Not found!</h1>
+    <p><Link to="/"><button>Go Back Home</button></Link></p></div>)
 }
 export default NotFound
